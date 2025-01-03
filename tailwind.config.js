f0da8482a8d405ddb34494f9e81abe0485731d7a
codecruz <codecruz.mt@gmail.com>
2025-01-03 18:22:05 +0100

    ✨ Added local TailwindCSS compiled sstyles to new hiragana chhart

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{html,js}"], // Ruta a tus archivos HTML/JS
+  content: [
+    "./public/**/*.html", // Asegúrate de incluir todos los archivos HTML
+    "./src/**/*.{js,jsx,ts,tsx}", // Incluye archivos JS/TS si los tienes
+  ],
   theme: {
     extend: {},
   },
